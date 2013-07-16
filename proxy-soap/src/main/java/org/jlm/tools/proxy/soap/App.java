63be604e859ca35fb0f602004b35adfc95541e59
nomadeous <jlamande@gmail.com>
2013-07-16 08:22:54 +0200

    Import

---
+++ proxy-soap/src/main/java/org/jlm/tools/proxy/soap/App.java
@@ -0,0 +1,13 @@
+package org.jlm.tools.proxy.soap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
